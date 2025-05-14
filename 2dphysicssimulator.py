562bebc42ff0c1ef63cd32b24cec64e1c8f1f78d
Kelly Li <kellyli838@gmail.com>
2025-05-14 14:58:37 -0400

    added backdrop

--- 2dphysicssimulator.py
+++ 2dphysicssimulator.py
@@ -12,14 +12,17 @@ class App:
         self.size = self.width, self.height = 640, 400
         self._display = display.set_mode(self.size)
         self.fpsLimit = 60
+        self._backdrop = pygame.image.load('2824536.jpg')
 
     def exitGame(self):
         pygame.quit()
+        sys.exit()
     
     def execute(self):
         while (self._running):
             clock.tick(self.fpsLimit)
             display.update()
+            self._display.blit(self._backdrop, (0,0))
 
         self.exitGame()
 
